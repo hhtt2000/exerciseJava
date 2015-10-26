ad8e8688af61323cf82f10b0abad9e9debbd122d
hhtt2000 <hhtt2000@hanmail.net>
2015-10-26 16:19:06 +0900

    indenttttt

--- java8/FlatMapTest.java
+++ java8/FlatMapTest.java
@@ -13,23 +13,23 @@ public class FlatMapTest {
 		//[java.util.stream.ReferencePipeline$Head@4a574795, java.util.stream.ReferencePipeline$Head@f6f4d33]
 		//각 단어(Hello)를 split해서 생긴 string(H,e,l,l,o)이 각각의 stream에 저장.
 		List<Stream<String>> collect = words.stream()
-														.map(word -> word.split(""))
-														.map(Arrays::stream)
-														.distinct()
-														.collect(Collectors.toList());
+				                            .map(word -> word.split(""))
+				                            .map(Arrays::stream)
+				                            .distinct()
+				                            .collect(Collectors.toList());
 		
 		//[[Ljava.lang.String;@3f99bd52, [Ljava.lang.String;@4f023edb]
 		List<String[]> collect2 = words.stream()
-												.map(word -> word.split(""))
-												.distinct()
-												.collect(Collectors.toList());
+				.map(word -> word.split(""))
+				.distinct()
+				.collect(Collectors.toList());
 		
 		//[H, e, l, o, W, r, d]
 		List<String> collect3 = words.stream()
-											.map(word -> word.split(""))
-											.flatMap(Arrays::stream)
-											.distinct()
-											.collect(Collectors.toList());
+				.map(word -> word.split(""))
+				.flatMap(Arrays::stream)
+				.distinct()
+				.collect(Collectors.toList());
 
 	}
 
